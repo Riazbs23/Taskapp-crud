543f20b5156ae507a00f13b7bde8cf42bb64d626
Riazbs23 <riazsorkar709@gmail.com>
2025-07-21 12:29:56 +0600

    Add project files.

---
+++ TaskApp/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskApp.Models;
+
+namespace TaskApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Task>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id)
+                      .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Title)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(e => e.DueDate)
+                      .IsRequired();
+
+                entity.Property(e => e.Status)
+                      .IsRequired()
+                      .HasDefaultValue("Not Started");
+            });
+        }
+    }
+}
---
+++ TaskApp/Models/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskApp.Models
+{
+    public class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } // Make sure this has both getter and setter
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Due date is required")]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public string Status { get; set; } = "Not Started";
+
+        // Constructor to initialize default values
+        public Task()
+        {
+            Status = "Not Started";
+        }
+    }
+}
